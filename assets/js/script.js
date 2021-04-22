505cfddb6a5a8c0ef2adb943c79b6251cfe91f9f
Jason <nyjasonlam@gmail.com>
2021-04-22 00:47:27 -0400

    Added populateTimeBlocksFromArray()

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,9 @@ var currentDayEl = $("#currentDay")
 // STARTING VALUES
 var currentDate = moment().format("dddd, MMMM Do YYYY");
 var currentTime = new Date().getHours();
-console.log(currentDate)
-var events = [];
-
+var dateStamp = moment().format("MMDDYY");
+var events = localStorage.getItem(dateStamp) ? JSON.parse(localStorage.getItem(dateStamp)) : []
+console.log(events)
 // FUNCTIONS
 function renderTimeBlock() {
     for (var i = 9; i < 18; i++) {
@@ -37,18 +37,46 @@ function checkIfEventPassed() {
             timeBlocksEls.eq(i).addClass("present")
         }
     }
-    
 }
 
 function renderCurrentDate() {
     currentDayEl.text(`Today is ${new Date().toDateString()}`)
 }
 
+function saveEventToArray(event) {
+    var eventText = $(event.target).parent().children("textarea").val()
+    var eventIndex = $(event.target).parent().attr("data-hour")
+    events[eventIndex - 9] = eventText
+    // console.log(events)
+}
+
+function saveArrayToLocalStorage() {
+    localStorage.setItem(dateStamp, JSON.stringify(events));
+    console.log(localStorage.getItem(dateStamp))
+}
+
+function populateTimeBlocksFromArray() {
+    for (var i = 0; i < 10; i++) {
+        if (events[i]) {
+            $(".time-block").eq(i).children("textarea").val(events[i])
+        }
+    }
+}
+
+function handleSaveBtnClick(event) {
+    saveEventToArray(event);
+    saveArrayToLocalStorage()
+}
+
+// console.log(events)
+
 renderCurrentDate();
 renderTimeBlock()
+populateTimeBlocksFromArray();
 // render row elements
     // check if events exist in localstorage
         // if they exist populate to corresponding row elements
         // render row elements from 9a to 5p
 
 // USER INTERACTIONS
+$("button").click(handleSaveBtnClick)
